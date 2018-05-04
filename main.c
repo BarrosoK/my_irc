b21f38206e5f3231c5d5c1e19ac9f5085f3d877d
Barroso <kevin.barroso@epitech.eu>
2018-05-04 15:49:45 +0200

    list cmd

--- main.c
+++ main.c
@@ -4,31 +4,33 @@
 #include "vector.h"
 #include "server.h"
 
-void *listen_paquet(void *para)
+void *send_msg_to_all(void *para)
 {
 	struct conn_s *conn = (struct conn_s *)para;
 	server_t *server = conn->server;
 
 	while (server->get_paquet(server, conn->conn_fd) == 0) {
+		if (strcasecmp(server->buffer, "list\n") == 0) {
+			dprintf(conn->conn_fd, "Players connected : %d\n", (int)server->connfd->size);
+			continue;
+		}
 		for (size_t i = 0; i < server->connfd->size; i++) {
 			if (conn->conn_fd !=
 				(int)server->connfd->at(server->connfd, i))
-			dprintf((int)server->connfd->at(server->connfd, i), "-> %s", server->buffer);
+				dprintf((int)server->connfd->at(server->connfd,
+					i), "-> %s", server->buffer);
 		}
 	}
-	free(conn);
 	printf("Client disconnected\n");
+	free(conn);
+	return (NULL);
 }
 
 int main()
 {
-	vector_t *vector = new_vector();
 	server_t *server = new_server();
 
-	vector->push_back(vector, (void *)server);
-
 	server->open_port(server, 30000);
-	server->start_listening(server, 5, THREAD, listen_paquet);
-
+	server->start_listening(server, 5, THREAD, send_msg_to_all);
 	return (0);
 }
